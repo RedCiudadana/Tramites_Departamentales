b0bcf854e14edd115d0768194502ba16e57e413a
MarianoReyes <josemarianoreyesh02@gmail.com>
2022-10-27 09:23:43 -0600

    backend funcional

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,10 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.setTemplateFormats("njk,html,md");
     
     eleventyConfig.addPassthroughCopy('src');
-    eleventyConfig.addPassthroughCopy('assets');
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('img');
     eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('admin');
 
     eleventyConfig.addNunjucksFilter("limit", function (array, limit) {
         return array.slice(0, limit);
